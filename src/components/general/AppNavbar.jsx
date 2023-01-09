446e1209fe845dd0d7d1540a253622bf863bd9ba
Nhật Huy ★彡 <provcc143300@gmail.com>
2023-01-09 12:26:12 +0700

    style Navbar

---
+++ src/components/general/AppNavbar.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { useLocation } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import "./generalComponent.css";
+import { Button } from "react-bootstrap";
+import { BsFillPersonLinesFill, BsBoxArrowRight } from "react-icons/bs";
+import TooltipTrigger from "./TooltipTrigger";
+
+function getActiveKey(pathname) {
+  const presentLinks = ["/presentations", "/slide"];
+  return presentLinks.includes(pathname) ? "presentations" : pathname;
+}
+
+export default function AppNavBar({ user, buttonSignOutClicked }) {
+  const location = useLocation();
+
+  const extraNavElements = user.users_id !== "" && (
+    <>
+      <Nav.Link href="/group">Group</Nav.Link>
+      <Nav.Link href={`/presentations/mypresent/${user.users_id}`}>
+        Presentations
+      </Nav.Link>
+    </>
+  );
+  const authElements =
+    user.users_id !== "" ? (
+      <>
+        <Nav.Link href="/profile">
+          <TooltipTrigger text={user.users_name}>
+            <Button variant="outline-primary">
+              <BsFillPersonLinesFill size="1.2em" />
+            </Button>
+          </TooltipTrigger>
+        </Nav.Link>
+        <Nav.Link style={{ marginLeft: "-.5rem" }}>
+          <TooltipTrigger text="Sign out">
+            <Button variant="outline-danger" onClick={buttonSignOutClicked}>
+              <BsBoxArrowRight />
+            </Button>
+          </TooltipTrigger>
+        </Nav.Link>
+      </>
+    ) : (
+      <>
+        <Nav.Link href="/login">Login</Nav.Link>
+        <Nav.Link href="/register">Sign up</Nav.Link>
+      </>
+    );
+  const activeKey = getActiveKey(location.pathname);
+
+  return (
+    <Navbar collapseOnSelect expand="md" bg="light" variant="light">
+      <Container>
+        <Navbar.Brand href="/">
+          <img
+            alt="present&study logo"
+            src="/logo.png"
+            style={{ height: "50px" }}
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto" activeKey={activeKey}>
+            <Nav.Link href="/">Home</Nav.Link>
+            {extraNavElements}
+          </Nav>
+          <Nav>{authElements}</Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,4 @@
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import { Link, Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -13,13 +10,12 @@ import JoinGroupByLink from "../group/joinGroup/JoinGroupByLink";
 import Presentation from "../presentation/Presentation";
 import Slide from "../slide/Slide";
 import MyPresentation from "../presentation/MyPresentation";
-
-import homeImage from "./home-button.png";
 import MemberView from "../slide/MemberView";
 
 import { JoinRoomSocket } from "./JoinRoomSocket";
 import Chat from "../chat/Chat";
 import NotifyChat from "./NotifyChat";
+import AppNavBar from "../general/AppNavbar";
 
 export default function Home() {
   const [user, setUser] = useState({
@@ -29,7 +25,7 @@ export default function Home() {
   });
   const navigate = useNavigate();
 
-  const buttonSignOut_Clicked = () => {
+  const buttonSignOutClicked = () => {
     const newUser = {
       users_id: "",
       users_name: "",
@@ -50,14 +46,9 @@ export default function Home() {
         }
       });
 
-      await axiosIntance
-        .get(`/auth/profile`)
-        .then((res) => {
-          setUser(res.data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      await axiosIntance.get(`/auth/profile`).then((res) => {
+        setUser(res.data);
+      });
     }
   };
 
@@ -67,76 +58,7 @@ export default function Home() {
 
   return (
     <div>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand>
-            <Link to="/">
-              <img alt="home" src={homeImage} style={{ height: "50px" }} />
-            </Link>
-          </Navbar.Brand>
-          <Nav className="me-auto">
-            <Navbar.Brand>
-              <Link className="textstyle" to="/">
-                Home
-              </Link>
-            </Navbar.Brand>
-            {user.users_id !== "" && (
-              <Navbar.Brand>
-                <Link className="textstyle" to="/group">
-                  My group
-                </Link>
-              </Navbar.Brand>
-            )}
-            {user.users_id !== "" && (
-              <Navbar.Brand>
-                <Link className="textstyle" to="/profile">
-                  My profile
-                </Link>
-              </Navbar.Brand>
-            )}
-            {user.users_id !== "" && (
-              <Navbar.Brand>
-                <Link
-                  className="textstyle"
-                  to={`/presentations/mypresent/${user.users_id}`}
-                >
-                  My presentations
-                </Link>
-              </Navbar.Brand>
-            )}
-          </Nav>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            {user.users_id !== "" ? (
-              <div>
-                <div className="allign1" onClick={buttonSignOut_Clicked}>
-                  <Navbar.Text>
-                    <div className="textstyle box1">{user.users_name}</div>
-                  </Navbar.Text>
-                </div>
-                <div className="allign1" onClick={buttonSignOut_Clicked}>
-                  <Navbar.Text>
-                    <div className="textstyle box1">Sign Out</div>
-                  </Navbar.Text>
-                </div>
-              </div>
-            ) : (
-              <div>
-                <Navbar.Text>
-                  <Link className="textstyle box1" to="/login">
-                    Sign In
-                  </Link>
-                </Navbar.Text>
-                <Navbar.Text>
-                  <Link className="textstyle box1" to="/register">
-                    Sign Up
-                  </Link>
-                </Navbar.Text>
-              </div>
-            )}
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <AppNavBar user={user} buttonSignOutClicked={buttonSignOutClicked} />
       <Routes>
         <Route index element={<Welcome />} />
         <Route path="group/*" element={<Group userId={user.users_id} />} />
